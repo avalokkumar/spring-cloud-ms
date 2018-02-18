b1e5ddda69a957b294d70c2ed522bc6dc841942c
avalokkumar <avalokkumar@gmail.com>
2018-02-18 22:35:28 +0530

    oauth service updated to generate token for authentication

--- oauth-service/src/main/java/com/claycorp/nexstore/api/OauthServiceApplication.java
+++ oauth-service/src/main/java/com/claycorp/nexstore/api/OauthServiceApplication.java
@@ -1,11 +1,15 @@
 package com.claycorp.nexstore.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
 @EnableAuthorizationServer
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 public class OauthServiceApplication {
 
 	public static void main(String[] args) {
